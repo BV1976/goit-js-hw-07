418a23687eff648eab97f4017e2a8c811a64eacc
Volodymyr <zahidbv@ukr.net>
2025-01-03 22:53:17 +0300

    add all

---
+++ js/task-6.js
@@ -0,0 +1,35 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = Number(input.value);
+
+  if (amount < 1 || amount > 100 || isNaN(amount)) {
+    alert('Please enter a number between 1 and 100');
+    return;
+  }
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    div.style.width = 30 + i * 10 + 'px';
+    div.style.height = 30 + i * 10 + 'px';
+    div.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(div);
+  }
+
+  input.value = '';
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
